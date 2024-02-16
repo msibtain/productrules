fa8078a181d102a7872720b47905421a2c46bc3c
Muhammad Sibtain <backened@slidesigma.com>
2024-02-16 22:42:41 +0500

    code cleanup

---
+++ translations/de.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{productrules}prestashop>productrules_d6e6cb19e3b9c02f89d6cd54cfa7c613'] = 'Geschäft';
+$_MODULE['<{productrules}prestashop>productrules_184daff620aca4ad23fd2bf70d0b76af'] = 'Persönlich';
+$_MODULE['<{productrules}prestashop>productrules_06d960789f90fb6cfd52bc1a905fd36d'] = 'Konto Typ';
+$_MODULE['<{productrules}prestashop>productrules_0823c44a8108610f054f66b6a6d31164'] = 'EORI Number';
